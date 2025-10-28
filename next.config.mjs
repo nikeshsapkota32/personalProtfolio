21fe241698bd59d452f4f9bcca5bc91dcff5fff8
NikeshSapkota <nikeshsapkota.code@gmail.com>
2025-10-28 10:40:54 -0500

    Configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/personalProtfolio',
+  assetPrefix: '/personalProtfolio/',
   typescript: {
     ignoreBuildErrors: true,
   },
